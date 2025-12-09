bd3c82bd828185ec4d7352051416f0be970167f8
Suvankar <suvankarpaul004@gmail.com>
2025-12-09 08:50:35 +0530

    data downloaded from s3 successfully in notebook

---
+++ main.py
@@ -0,0 +1,3 @@
+from src.cnnClassifier import logger  # Adjusted the import path to include the 'src' directory
+
+logger.info("Starting the CNN Classifier application.")
---
+++ setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+# This code reads the README.md file and uses it as the long description for the package.
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+
+__version__ = "0.0.0"
+
+REPO_NAME = "End-to-End-Deep-Learning-Project-Chicken-Disease"
+AUTHOR_USER_NAME = "suvankar"
+SRC_REPO = "cnnClassifier"
+AUTHOR_EMAIL = "suvankarpaul004@gmail.com"
+
+
+setuptools.setup(
+    name=SRC_REPO,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="A small python package for CNN app",
+    long_description=long_description,
+    long_description_content="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls={
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+    },
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src")
+)
