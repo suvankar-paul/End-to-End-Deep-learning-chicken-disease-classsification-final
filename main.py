48385ee7e157937eb477185c94a0c343346562e8
Suvankar <suvankarpaul004@gmail.com>
2025-12-11 04:53:36 +0530

    model training done

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from src.cnnClassifier import logger  # Adjusted the import path to include the 'src' directory
 from src.cnnClassifier.pipeline.data_ingestion import DataIngestionTrainingPipeline
 from src.cnnClassifier.pipeline.prepare_base_model import PrepareBaseModelTrainingPipeline
+from src.cnnClassifier.pipeline.model_trainer import ModelTrainingPipeline
 from dotenv import load_dotenv
 
 load_dotenv()  # Load environment variables from .env file
@@ -24,6 +25,20 @@ try:
    prepare_base_model = PrepareBaseModelTrainingPipeline()
    prepare_base_model.main()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+
+STAGE_NAME = "Training the model"
+try:  
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   from src.cnnClassifier.pipeline.model_trainer import ModelTrainingPipeline
+   model_trainer = ModelTrainingPipeline()
+   model_trainer.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+
 except Exception as e:
         logger.exception(e)
         raise e
---
+++ src/cnnClassifier/pipeline/model_trainer.py
@@ -0,0 +1,22 @@
+from cnnClassifier.config.configuration import ConfigurationManager
+from cnnClassifier.components.model_trainer import Training
+from cnnClassifier import logger
+
+
+
+
+STAGE_NAME = "Training"
+
+
+
+class ModelTrainingPipeline:
+    def __init__(self):
+        pass
+
+    def main(self):
+        config = ConfigurationManager()
+        training_config = config.get_training_config()
+        training = Training(config=training_config)
+        training.get_base_model()
+        training.train_valid_generator()
+        training.train()
